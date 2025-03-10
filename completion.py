48750ae3ac451088d2b93184162046495be4feb9
Collin Harrington <collin1234harr@gmail.com>
2025-03-10 18:17:34 -0400

    added "default_model"

--- completion.py
+++ completion.py
@@ -4,21 +4,23 @@ from tracking import default_client, read_usage
 from typing import List
 import re
 
-def complete(client, s: str, model="gpt-3.5-turbo-0125", *args, **kwargs):
+default_model = "gpt-3.5-turbo-0125"
+
+def complete(client, s: str, model=default_model, *args, **kwargs):
     response = client.chat.completions.create(messages=[{"role": "user", "content": s}],
                                               model=model,
                                               temperature=0.6,
                                               *args, **kwargs)
     return [choice.message.content for choice in response.choices][0]
 
-def call_llm_gpt4o_mini(prompts: List[str], *args, **kwargs) -> List[str]:
+def call_gpt4o_mini(prompts: List[str], *args, **kwargs) -> List[str]:
     results = []
     for prompt in prompts:
         escaped = re.sub('"', '\"', prompt)
         results.append(complete(default_client, escaped, model="gpt-4o-mini"))
     return results
 
-def call_llm_gpt3_5(prompts: List[str], *args, **kwargs) -> List[str]:
+def call_gpt3_5(prompts: List[str], *args, **kwargs) -> List[str]:
     results = []
     for prompt in prompts:
         escaped = re.sub('"', '\"', prompt)
